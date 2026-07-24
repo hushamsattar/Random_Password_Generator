3d90340f2cd30b63d27887333c730d42f30a20c2
hushamsattar <husham.j.sattar@gmail.com>
2026-07-24 11:04:10 -0500

    initial commit

---
+++ password_gen.py
@@ -0,0 +1,18 @@
+# 6 – Random Password generator
+
+import secrets
+import string
+
+
+def generate_secure_password(length=16):
+    characters = (
+        string.ascii_letters +
+        string.digits +
+        string.punctuation
+    )
+
+    return ''.join(secrets.choice(characters) for _ in range(length))
+
+
+print(generate_secure_password())
+
